df5ce66c334f5b2317ff48efd12f8621f939bc2b
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-10-11 22:59:53 -0300

    feat: Add error handling middleware to the server

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,28 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
-import routes from './router';  // Certifique-se de que a importação esteja correta
+import routes from './router';
+import AppError from '../erros/AppError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+// Middleware para tratamento de erros
+app.use((error: AppError | Error, request: Request, response: Response, next: NextFunction): void => {
+  if (error instanceof AppError) {
+    response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  } else {
+    response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  }
+});
 
 app.listen(3333, () => {
   console.log("Server is running on port 3333! 🚀");
