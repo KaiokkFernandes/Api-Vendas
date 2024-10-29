07ab30f813a781e5971cba09317f1beb72008376
KaiokkFernandes <kaiofernandess654@gmail.com>
2024-10-28 23:08:39 -0300

    treatments data for erros with celebrate aind join

--- src/modules/product/routes/products.router.ts
+++ src/modules/product/routes/products.router.ts
@@ -1,14 +1,25 @@
 import { Router } from 'express';
 import ProductController from '../controller/ProductController';
 import asyncHandler from '@config/Middleware';
+import {celebrate, Joi, Segments} from 'celebrate'
 
 const productRouter = Router();
 const productsController = new ProductController();
 
 productRouter.get('/', asyncHandler(productsController.index.bind(productsController)));
-productRouter.get('/:id', asyncHandler(productsController.show.bind(productsController)));
+
+productRouter.get('/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required()
+    }
+  }),
+ asyncHandler(productsController.show.bind(productsController)));
+
 productRouter.post('/', asyncHandler(productsController.create.bind(productsController)));
+
 productRouter.put('/:id', asyncHandler(productsController.update.bind(productsController)));
+
 productRouter.delete('/:id', asyncHandler(productsController.delete.bind(productsController)));
 
 export default productRouter;
