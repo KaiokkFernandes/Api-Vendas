507f033210e5a55f40af022f1d7e7ed0fb2bac6f
KaiokkFernandes <kaiofernandess654@gmail.com>
2025-03-04 23:51:59 -0300

    adicionando rotas para gerenciamento de sessões de autenticação

--- src/shared/http/router/index.ts
+++ src/shared/http/router/index.ts
@@ -1,8 +1,13 @@
 import productRouter from '@modules/product/routes/products.routes';
+import sessionsRouter from '@modules/users/routes/session.routes';
+import UsersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/products', productRouter);
+routes.use('/users', UsersRouter);
+routes.use('/sessions', sessionsRouter);
+
 
 export default routes;
