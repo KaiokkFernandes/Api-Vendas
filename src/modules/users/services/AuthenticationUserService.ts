f7aa1e16cc9b573eafb82edb4a5840aed3fa9729
KaiokkFernandes <kaiofernandess654@gmail.com>
2025-03-05 00:00:21 -0300

    adicionando suporte a autenticação com JWT

--- src/modules/users/services/AuthenticationUserService.ts
+++ src/modules/users/services/AuthenticationUserService.ts
@@ -3,16 +3,21 @@ import { getCustomRepository } from "typeorm";
 import Users from "../typeorm/entities/Users";
 import UserRepository from "../typeorm/repositories/UserRepository";
 import { compare, hash } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 
 interface IRequest{
   email: string;
   password: string;
 }
+interface IResponse {
+  user: Users;
+  token: string;
+}
 
 
 class AuthenticationUserService {
-  public async execute({ email, password, }: IRequest): Promise<Users>{
+  public async execute({ email, password, }: IRequest): Promise<IResponse>{
     const usersRepository = getCustomRepository(UserRepository);
 
     const user = await usersRepository.findByEmail(email);
@@ -26,7 +31,14 @@ class AuthenticationUserService {
     throw new AppError("Incorret password, please try again.", 401);
    }
 
-     return user;
+   const token = sign({}, 'f296f1', {
+    subject: user.id,
+    expiresIn: '1d'
+   })
+     return {
+        user,
+        token
+     };
   }
 
 }
