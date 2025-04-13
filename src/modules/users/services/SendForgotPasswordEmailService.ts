7ff5e41e9f7e121930425fa81dd0f8284cf88782
KaiokkFernandes <kaiofernandess654@gmail.com>
2025-04-13 19:03:17 -0300

    adicionando serviço para envio de e-mail de recuperação de senha

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,27 @@
+import AppError from "@shared/erros/AppError";
+import { getCustomRepository } from "typeorm";
+import UserRepository from "../typeorm/repositories/UserRepository";
+import UserTokenRepository from "../typeorm/repositories/UserTokenRepository";
+
+interface IRequest{
+    email: string;
+}
+
+class SendForgotPasswordEmailService{
+   public async execute({ email}: IRequest): Promise<void>{
+     const usersRepository = getCustomRepository(UserRepository);
+     const userTokenRepository = getCustomRepository(UserTokenRepository);
+
+     const user = await usersRepository.findByEmail(email);
+
+     if(!user){
+        throw new AppError('User does not exists');
+     }
+     const token =  await userTokenRepository.generate(user.id);
+     console.log(token);
+   }
+
+}
+
+
+export default SendForgotPasswordEmailService;
