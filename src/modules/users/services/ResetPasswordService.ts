98c594f44bb2d6342d86ffcf4e5c2f9ceecc5ce4
KaiokkFernandes <kaiofernandess654@gmail.com>
2025-04-13 20:17:39 -0300

    adicionando serviço para redefinição de senha com validação de token e expiração

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,40 @@
+import AppError from "@shared/erros/AppError";
+import { getCustomRepository } from "typeorm";
+import UserRepository from "../typeorm/repositories/UserRepository";
+import UserTokenRepository from "../typeorm/repositories/UserTokenRepository";
+import {isAfter, addHours} from 'date-fns';
+import { hash } from "bcryptjs";
+
+interface IRequest{
+    token: string;
+    password: string;
+}
+
+class ResetPasswordService{
+   public async execute({ token, password}: IRequest): Promise<void>{
+     const usersRepository = getCustomRepository(UserRepository);
+     const userTokenRepository = getCustomRepository(UserTokenRepository);
+
+     const userToken = await userTokenRepository.findByToken(token);
+
+     if(!userToken){
+        throw new AppError('User does not exists');
+     }
+     const user = await usersRepository.findById(userToken.user_id);
+
+     if(!user){
+      throw new AppError('User does not exists');
+   }
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if(isAfter(Date.now(), compareDate)){
+      throw new AppError('Token expired');
+    }
+    user.password = await hash(password, 8);
+   }
+
+}
+
+
+export default ResetPasswordService;
