79f4882570ae5791f9da9ee18556b9cc9bc80e29
KaiokkFernandes <kaiofernandess654@gmail.com>
2025-05-11 15:13:44 -0300

    adicionando rota para redefinição de senha com validação de token e confirmação de senha

--- src/modules/users/routes/password.routes.ts
+++ src/modules/users/routes/password.routes.ts
@@ -22,4 +22,14 @@ celebrate({
 asyncHandler(forgotPasswordController.create));
 
 
+passwordRoutes.post('/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  asyncHandler(forgotPasswordController.create));
+
 export default passwordRoutes;
