2574d2005e9757c2c467c4e96492e219aca271b7
KaiokkFernandes <kaiofernandess654@gmail.com>
2025-05-11 17:04:32 -0300

    ajustando a interface de template de e-mail para usar 'file' em vez de 'template' e corrigindo a lógica de envio de e-mail de recuperação de senha

--- src/config/mail/HandleBarsMailTemplate.ts
+++ src/config/mail/HandleBarsMailTemplate.ts
@@ -1,4 +1,6 @@
 import Handlebars from "handlebars";
+import fs from "fs";
+import path from "path";
 
 interface ITemplateVariable{
   [key: string]: string | number;
@@ -6,16 +8,22 @@ interface ITemplateVariable{
 
 
 interface IMailTemplate {
-  template: string;
+  file: string;
   variables: ITemplateVariable;
 }
 
 
 export class handlebarsMailTemplate {
   public async parse({
-    template,
+    file,
     variables,
   }: IMailTemplate): Promise<string> {
+
+  const templateContent = await fs.promises.readFile(
+    path.resolve(file),
+    { encoding: 'utf-8' }
+  );
+    const template = Handlebars.compile(templateContent);
     const parseTemplate = Handlebars.compile(template);
 
     return parseTemplate(variables);
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,6 +3,8 @@ import { getCustomRepository } from "typeorm";
 import UserRepository from "../typeorm/repositories/UserRepository";
 import UserTokenRepository from "../typeorm/repositories/UserTokenRepository";
 import EtherealMail from "@config/mail/EtherealMail";
+import path from 'path';
+import { link } from "joi";
 
 interface IRequest{
     email: string;
@@ -22,6 +24,8 @@ class SendForgotPasswordEmailService{
 
      const token =  await userTokenRepository.generate(user.id);
 
+     const forgotPasswordTemplate = path.resolve(__dirname, '..', 'views', 'forgot_password.hbs');
+
      await EtherealMail.sendMail({
         to: {
             name: user.name,
@@ -29,10 +33,10 @@ class SendForgotPasswordEmailService{
         },
         subject: '[API Vendas] Recuperação de senha',
         templateData:{
-            template: `olá {{name}}: {{token}}`,
+            file: forgotPasswordTemplate,
             variables:{
               name: user.name,
-              token : token?.token,
+              link: `http:/localhost:3000/reset_password?token=${token.token}`,
             }
         }
      })
