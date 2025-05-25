0c7f404790f0ada179c1e41e68ede1b41bb33a07
KaiokkFernandes <kaiofernandess654@gmail.com>
2025-05-25 14:45:20 -0300

    Create service showPerfil

---
+++ src/modules/users/services/ShowPerfil.ts
@@ -0,0 +1,24 @@
+import AppError from "@shared/erros/AppError";
+import { getCustomRepository } from "typeorm";
+import Users from "../typeorm/entities/Users";
+import { create } from "domain";
+import UserRepository from "../typeorm/repositories/UserRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ShowProfileServices {
+   public async execute({user_id}: IRequest): Promise<Users>{
+   const usersRepository = getCustomRepository(UserRepository);
+
+   const users = await usersRepository.findById(user_id);
+
+   if(!users) {
+    throw new AppError('User not found');
+   }
+   return users;
+
+  }
+}
+export default ShowProfileServices;
